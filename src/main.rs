11d3c9f79981b17da6e9d15f5c5ec47601c66b16
887 <2300887@gmail.com>
2016-10-13 00:25:19 +0200

    Not sure how to handle UDP with futures, need to dig deeper into how they actually work

--- src/main.rs
+++ src/main.rs
@@ -7,48 +7,56 @@ use std::net::SocketAddr;
 use futures::Future;
 use futures::stream::Stream;
 use tokio_core::io::{copy, Io};
-use tokio_core::net::TcpListener;
+use tokio_core::net::{TcpListener, UdpSocket};
 use tokio_core::reactor::Core;
 
-//https://tokio-rs.github.io/tokio-core/tokio_core/index.html
-//the tokio-rs example could make a solid foundation, need to investigate it more
-
+// https://tokio-rs.github.io/tokio-core/tokio_core/index.html
+// the tokio-rs example could make a solid foundation, need to investigate it more
 
 fn main() {
-    let addr = env::args().nth(1).unwrap_or("127.0.0.1:8080".to_string());
-    let addr = addr.parse::<SocketAddr>().unwrap();
+    let tcp_addr = env::args().nth(1).unwrap_or("127.0.0.1:8080".to_string());
+    let udp_addr = env::args().nth(2).unwrap_or("127.0.0.1:8082".to_string());
+
+    let tcp_addr = tcp_addr.parse::<SocketAddr>().unwrap();
+    let udp_addr = udp_addr.parse::<SocketAddr>().unwrap();
 
     // Create the event loop that will drive this server
     let mut l = Core::new().unwrap();
-    let handle = l.handle();
 
-    // Create a TCP listener which will listen for incoming connections
-    let socket = TcpListener::bind(&addr, &handle).unwrap();
+    let tcp_handle = l.handle();
+    let udp_handle = l.handle();
 
-    // Once we've got the TCP listener, inform that we have it
-    println!("Listening on: {}", addr);
+    let tcp_socket = TcpListener::bind(&tcp_addr, &tcp_handle).unwrap();
+    let udp_socket = UdpSocket::bind(&udp_addr, &udp_handle).unwrap();
 
-    // Pull out the stream of incoming connections and then for each new
-    // one spin up a new task copying data.
-    //
-    // We use the `io::copy` future to copy all data from the
-    // reading half onto the writing half.
-    let done = socket.incoming().for_each(|(socket, addr)| {
+    println!("tcp listening on: {}", tcp_addr);
+    println!("udp listening on: {}", udp_addr);
+
+    let tcp_socket_handler = tcp_socket.incoming().for_each(|(socket, addr)| {
         let pair = futures::lazy(|| Ok(socket.split()));
+        //copy data from reading to writing half
         let amt = pair.and_then(|(reader, writer)| copy(reader, writer));
-
-        // Once all that is done we print out how much we wrote, and then
-        // critically we *spawn* this future which allows it to run
-        // concurrently with other connections.
-        handle.spawn(amt.then(move |result| {
+        //spawn the future to allow it to run concurrently
+        tcp_handle.spawn(amt.then(move |result| {
             println!("wrote {:?} bytes to {}", result, addr);
             Ok(())
         }));
-
         Ok(())
     });
 
+    //TODO:
+    //there seems to be no tcp incoming counterpart for udp yet, and I probably shouldn't
+    //do actual socket reading/writing operations on the same thread I am listening for
+    //connections.
+    //here is an example with the threadpool api, i probably need this:
+    //https://docs.rs/futures-cpupool/0.1.2/futures_cpupool/
+    //also if I want to involve hyper later to parse the google https dns api and
+    //i also may need futures for that too?
+    //here is a more or less generic future example:
+    //http://alexcrichton.com/futures-rs/futures/index.html
+
+
     // Execute our server (modeled as a future) and wait for it to
     // complete.
-    l.run(done).unwrap();
+    l.run(tcp_socket_handler).unwrap();
 }
