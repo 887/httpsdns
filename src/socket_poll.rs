1f0db5eacb949b557cc2d9e5f2a83a69a393deff
887 <2300887@gmail.com>
2016-10-18 01:01:09 +0200

    no idea how to use the cpu pool

--- src/socket_poll.rs
+++ src/socket_poll.rs
@@ -12,6 +12,8 @@ use futures_cpupool::CpuPool;
 
 use socket_send::*;
 
+type DnsAnswer = [u8; 1500];
+
 pub struct SocketPoll {
     socket: UdpSocket,
     pool: CpuPool,
@@ -32,62 +34,34 @@ impl Future for SocketPoll {
 
     fn poll(&mut self) -> Poll<(), Self::Error> {
         loop {
+            log("socket polling..");
             let socket_poll_result = self.socket.poll_read();
             log("socket polled!");
             match socket_poll_result {
                 Async::Ready(_) => {
                     log("socket ready!");
 
-                    //the original design was to just have one buffer at runtime
-                    //but i want to hand of this buffer to a different thread latter so
-                    //it makes more sense to initialize it here so i can move its ownership later
                     let mut buffer = [0; 1500];
                     let (amt, addr) = try_nb!(self.socket.recv_from(&mut buffer));
 
-                    //TODO: we should probably model the answer as a future
-                    //and push it on the CPU pool
-                    //let ss = SocketSend {
-                    //socket: self.socket,
-                    //buffer: buffer;
-                    //adr: adr,
-                    //};
-                    //self.pool.spawn(SocketSend);
-                    //pool.spawn(recv/answer);
-
-                    log("socket data received!");
-                    if 0 == amt {
-                        //return Err(Error::new(ErrorKind::Other, "wrong read"));
-                        let mock: [u8; 3] = [1,2,3];
-                        let amt = try_nb!(self.socket.send_to(&mock, &addr));
-                        if 0 == amt {
-                            return Err(Error::new(ErrorKind::Other, "wrong write"));
-                        } else {
-                            log("socket answer mock send!");
-                        }
-                    } else {
-                        let amt = try_nb!(self.socket.send_to(&buffer[..amt], &addr));
-                        if 0 == amt {
-                            return Err(Error::new(ErrorKind::Other, "wrong write"));
-                        } else {
-                            log("socket answer echoed!");
-                        }
+                    //TODO: make this part work
+                    //let ss = SocketDnsRequest::new(buffer, amt, addr);
+                    //match self.pool.spawn(ss) {
+                        //Ok(answer) => {
+                            //match self.socket.send_to(&answer, &addr) {
+                              //Ok(amt) => {log("DNS request answered!");}
+                              //Err(_) => {log("Couldn't answer DNS request");}
+                            //}
+                        //},
+                        //_ => {}
+                    //}
+                    match self.socket.send_to(&buffer[..amt], &addr) {
+                        Ok(_) => {log("DNS request answered!");}
+                        Err(_) => {log("Couldn't answer DNS request");}
                     }
+
                 },
                 _ => {
-                    //this only happens once!
-                    //nice, no useless cpu cycles!
-                    //it reads in the docs to this:
-                    //If this function returns `Async::NotReady` then the current future's
-                    //task is arranged to receive a notification when it might not return
-                    //`NotReady`.
-                    //
-                    //ok this is pretty much whats needed here, although i have no idea WHY it
-                    //works and maybe it depends on platform specific details in the background ?
-                    //(linux epoll, windows iocp etc)
-                    //huh interesting according to
-                    //fn _run(&mut self, done: &mut FnMut() -> bool) {
-                    //this is epoll in linux but i don't know if it will be iocp in windows
-                    //impotant: still test the behavior as described on the TODO above
                     log("socket not ready!");
                     return Ok(Async::NotReady)
                 },
--- src/socket_send.rs
+++ src/socket_send.rs
@@ -1,9 +1,41 @@
+use std::io::{Error, ErrorKind};
+
+use futures::{Async, Future, Poll, BoxFuture};
+
+use chrono::{Local};
+
 use tokio_core::net::UdpSocket;
 use std::net::SocketAddr;
 
-pub struct SocketSend {
-    socket: UdpSocket,
+type DnsAnswer = String;
+
+pub struct SocketDnsRequest {
     buffer: [u8; 1500],
-    adr: SocketAddr,
+    amt: usize,
+    addr: SocketAddr,
 }
 
+impl SocketDnsRequest {
+    pub fn new(buffer: [u8; 1500], amt: usize, addr: SocketAddr) -> Self {
+        SocketDnsRequest {
+            buffer: buffer,
+            amt: amt,
+            addr: addr,
+        }
+    }
+}
+
+impl Future for SocketDnsRequest {
+    type Item = DnsAnswer;
+    type Error = Error;
+
+    fn poll(&mut self) -> Poll<DnsAnswer, Self::Error> {
+        //TODO parse dns request
+        //TODO do https web request to get DNS data
+        Ok((Async::Ready("answer".to_string())))
+    }
+}
+
+fn log(text: &str) {
+    println!("{}: {}", Local::now().to_string(), text);
+}
